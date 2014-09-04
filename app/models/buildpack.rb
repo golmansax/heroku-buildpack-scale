2ae4d815247f6f9fec89ce3391344d88a0c12e7c
Holman Gao <holman@golmansax.com>
2014-09-03 23:12:43 -0700

    Need to figure out how rspec works

--- app/models/buildpack.rb
+++ app/models/buildpack.rb
@@ -1,2 +1,9 @@
 class Buildpack < ActiveRecord::Base
+  def pretty_name
+    repo_name.match(/heroku-buildpack-(.*)/).captures.first.capitalize
+  end
+
+  def repo_name
+    File.basename(url, '.git')
+  end
 end
--- spec/models/buildpack_spec.rb
+++ spec/models/buildpack_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Buildpack, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Buildpack do
+  describe '#pretty_name' do
+    xit 'it is a capitalized suffix of the url' do
+    end
+  end
 end
