96b97f6e0729db8ce72667d06458095115249c2a
Holman Gao <holman@golmansax.com>
2014-09-08 21:01:05 -0700

    Adding components structure

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module HerokuBuildpackScale
     # config.i18n.load_path +=
     #   Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.assets.paths << Rails.root.join('assets', 'components')
   end
 end
