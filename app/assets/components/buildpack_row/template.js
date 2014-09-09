88672c505f1b0f8d609a867326b2e73c6b931608
Holman Gao <holman@golmansax.com>
2014-09-08 21:45:07 -0700

    Adding jshint task

--- app/assets/components/buildpack_row/template.js
+++ app/assets/components/buildpack_row/template.js
@@ -1,9 +1,11 @@
-define(['react', 'underscore'], function (React, _) {
+define(['react'], function (React) {
   'use strict';
 
   return function (state, props) {
     return React.DOM.tr(null,
-      React.DOM.td(null, props.pretty_name),
+      React.DOM.td(null,
+        React.DOM.a({ href: props.url }, props.pretty_name)
+      ),
       React.DOM.td(null, props.weight_in_mb)
     );
   }
