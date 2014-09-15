346519343d1339421772e9d67ff9a1b78120010b
Holman Gao <holman@golmansax.com>
2014-09-15 16:35:28 -0700

    Adding Sidekiq route and nginx conf

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails', '~> 4.1.0'
 gem 'sqlite3'
 gem 'jbuilder'
 gem 'sidekiq'
+gem 'sinatra', '>= 1.3.0', require: false
 
 gem 'sass-rails', '~> 4.0.0'
 gem 'uglifier', '>= 1.3.0'
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ HerokuBuildpackScale::Application.routes.draw do
   root 'pages#index'
 
   resources :buildpacks, only: [:index, :create, :update]
+
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
 end
