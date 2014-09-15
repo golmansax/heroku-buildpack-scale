49490f5ba377c25b675b7b5dd2cbd7453be5978d
Holman Gao <holman@golmansax.com>
2014-09-15 16:59:15 -0700

    Preparing route for production

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 HerokuBuildpackScale::Application.routes.draw do
-  root 'pages#index'
+  def set_routes
+    root 'pages#index'
 
-  resources :buildpacks, only: [:index, :create, :update]
+    resources :buildpacks, only: [:index, :create, :update]
 
-  require 'sidekiq/web'
-  mount Sidekiq::Web => '/sidekiq'
+    require 'sidekiq/web'
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
+  if Rails.env.production?
+    scope '/heroku-buildpack-scale' do
+      set_routes
+    end
+  else
+    set_routes
+  end
 end
